8d9ba898e94fc20611ee91ee3cc7bfbb07d2f25a
Travis-Sherman <trav62801@gmail.com>
2021-05-26 11:29:44 -0400

    comment lines

--- MODULE.py
+++ MODULE.py
@@ -1,8 +1,14 @@
 def triple(x):
+    """"triples number"""
+
     return x * 3
 
 def cubed(x):
+    """"Cubes nuber"""
+
     return x ** 3
 
 def addThenCubed(x, y):
+    """Adds both number and cubes the sum"""
+
     return (x + y) ** 3
