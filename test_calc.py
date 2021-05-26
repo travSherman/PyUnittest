f2aa29759e0ec2499e213778384ce5834a7c9364
Travis-Sherman <trav62801@gmail.com>
2021-05-26 13:04:30 -0400

    error cases updated

--- test_calc.py
+++ test_calc.py
@@ -4,27 +4,24 @@ import calc
 
 class testCalc(unittest.TestCase):
 
-    # 2 Test triple function to check input validation and calculations
-    def test_triple_false(self): 
-        self.assertEqual(calc.triple("shsh"), False, "Should be False")
-    
+    #Test triple function to check input validation and calculations
     def test_triple(self): 
         self.assertEqual(calc.triple(2), 6, "Should be 6")
 
-    # 2 Test cubed function to check input validation and calculations
-    def test_cubed_false(self):
-        self.assertEqual(calc.cubed("shhsh") , False, "Should be False")
+        self.assertRaises(ValueError, calc.triple, "2a")
+
+    #Test cubed function to check input validation and calculations
     
     def test_cubed(self):
         self.assertEqual(calc.cubed(3) , 27, "Should be 27")
 
-    # 3 Test addThen Cubed function to check input validation and calculations
-    def test_addThenCubed_false(self):
-        self.assertEqual(calc.addThenCubed("hsjj",6), False, "Should be False")
+        self.assertRaises(ValueError, calc.cubed, "!")
 
-    def test_addThenCubed_false2(self):
-        self.assertEqual(calc.addThenCubed(6, "hsjj"), False, "Should be False")
+    #Test addThen Cubed function to check input validation and calculations
     
     def test_addThenCubed2(self):
         self.assertEqual(calc.addThenCubed(4, 6), 1000, "Should be 1000")
+
+        self.assertRaises(ValueError, calc.addThenCubed, "gqyqwus", 2)
+        self.assertRaises(ValueError, calc.addThenCubed, 4, "")
  
